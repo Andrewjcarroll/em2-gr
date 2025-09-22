40ab95b5512292f4b6d47d35346880270a1077fa
andrewjcarroll <andrew.ajc.carroll@gmail.com>
2025-09-22 13:50:24 -0600

    Added Rk45 compatability

--- solver/include/grDef.h
+++ solver/include/grDef.h
@@ -34,7 +34,7 @@
      dsolve::SOLVER_OCTREE_MIN[2])
 
 // type of the rk method.
-enum RKType { RK3 = 0, RK4, RK45 };
+enum RKType { RK3 = 0, RK4, RK5, RK45_CASH_KARP,RK45   };
 
 namespace dsolve {
 // clang-format off
