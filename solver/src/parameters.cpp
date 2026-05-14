590a4fe49ee9625f1d111216eb4d6ce7ab3fd415
Andrew Carroll <andrew.ajc.carroll@gmail.com>
2026-05-14 10:24:18 -0600

    Raised maximum wavelet tolerance

--- solver/src/parameters.cpp
+++ solver/src/parameters.cpp
@@ -494,7 +494,7 @@ void readParamFile(const char* inFile, MPI_Comm comm) {
 
     if (file.contains("dsolve::SOLVER_WAVELET_TOL")) {
         if (0.0 > file["dsolve::SOLVER_WAVELET_TOL"].as_floating() ||
-            1e-04 < file["dsolve::SOLVER_WAVELET_TOL"].as_floating()) {
+            200.0 < file["dsolve::SOLVER_WAVELET_TOL"].as_floating()) {
             std::cerr << R"(Invalid value for "dsolve::SOLVER_WAVELET_TOL")"
                       << std::endl;
             exit(-1);
